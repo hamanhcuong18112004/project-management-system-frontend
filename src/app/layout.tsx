417f6e011e566eb29f84ed65aff97a4d00162ebc
Colenviem <hiennguyenbuitan@gmail.com>
2026-05-08 10:35:04 +0700

    feat: add notification integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Toaster } from "sonner";
 import "./globals.css";
 import AuthProvider from "@/providers/AuthProvider";
 import RealtimeProvider from "@/providers/RealtimeProvider";
+import { NotificationProvider } from "@/providers/NotificationProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +34,9 @@ export default function RootLayout({
       >
         <AuthProvider>
           <RealtimeProvider>
-            {children}
+            <NotificationProvider>
+              {children}
+            </NotificationProvider>
           </RealtimeProvider>
         </AuthProvider>
         <Toaster
